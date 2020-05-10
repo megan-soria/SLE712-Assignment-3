ee7020c335d8d45cf8e06d56490b1e1554a170d9
megan0012 <megan.m.soria@gmail.com>
2020-05-10 14:21:20 +1000

    RScript for part1 questions 1 to 5.
    Needs review, especially #5. Histogram doesn't look right

---
+++ Scripts/part1_1to5.R
@@ -0,0 +1,26 @@
+library(dplyr)
+
+# 1. Read in the file, making the gene accession numbers the row names. 
+#    Show a table of values for the first six genes.
+
+gene_expression_data <- read.csv("~/projects/SLE712-Assignment-3/Data/part1_gene_expression.tsv", 
+                                 sep = '\t', row.names = "GeneID")
+head(gene_expression_data, n=6)
+
+# 2. Make a new column which is the mean of the other columns. 
+#    Show a table of values for the first six genes.
+
+gene_expression_data$Mean <- rowMeans(gene_expression_data[,1:2])
+head(gene_expression_data, n=6)
+
+# 3. List the 10 genes with the highest mean expression
+top10_mean <- top_n(gene_expression_data, 10, Mean)
+top10_mean
+
+# 4. Determine the number of genes with a mean <10
+lessthan10 <- gene_expression_data[(gene_expression_data[,3]<10),]
+nrow(lessthan10)
+
+# 5. Make a histogram plot of the mean values in png format and paste it into your report.
+hist(gene_expression_data$Mean)
+
