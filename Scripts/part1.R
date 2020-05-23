573d3c076c21918d776780939ca00ec91a0946b4
megan0012 <megan.m.soria@gmail.com>
2020-05-23 19:43:27 +1000

    Added the README.html Changed the script for part1, item 5 to adjust the histogram Created png files for the histograms

--- Scripts/part1.R
+++ Scripts/part1.R
@@ -16,13 +16,43 @@ gene_ordered <- gene_expression_data[order(-gene_expression_data$Mean),]
 head(gene_ordered, 10)
 
 # 4. Determine the number of genes with a mean <10
-lessthan10 <- gene_expression_data[(gene_expression_data[,3]<10),]
+mean_lessthan10 <- gene_expression_data[(gene_expression_data[,3]<10),]
 nrow(lessthan10) # 43124 genes
 
 # 5. Make a histogram plot of the mean values in png format and paste it into your report.
-png(filename = "Data/part1_histogram" )
-hist(gene_expression_data$Mean, breaks = 5, xlab = "Mean", main = "Histogram of Gene Expression Data Mean")
+png(filename = "Data/part1_histogram.png")
+hist(gene_expression_data$Mean, xlab = "Mean", main = "Histogram of Gene Expression Data Mean")
 dev.off()
+
+# The histogram for the Mean column produces only one bar graph near Mean = zero. 
+# This tells us that the number of genes with mean=0 & mean<500 far exceeds the number of genes with, 
+# say, mean =< 500. The frequency is skewed to the right and does not give us a proper picture of the data. 
+# We can solve this by subsetting the Mean column and taking out the extremes. 
+
+# To inspect the data, we can use the ftable() function to give us the numerical data of the
+# mean frequencies
+
+ftable(gene_expression_data$Mean)
+
+# -----Console Result-----
+#     0   0.5     1   1.5     2   2.5     3   3.5     4   4.5     5
+# 29694  4269  2130  1392   976   766   586   517   388   367   298
+
+mean(gene_expression_data$Mean) # column mean = 354.5272
+
+# From the result above, we notice that our observation was correct and the count for mean = 0 skews the data.
+# In the script below, the mean of the column was set as the maximum (354.5272 ~ 360) and only mean values greater than 2 is 
+# set as minimum. By removing the extremes, the histogram can now reveal how the frequency of the mean steeply
+# drops after about the first 10 bars.
+
+png(filename = "Data/part1_histogram_adjusted.png")
+with(gene_expression_data, hist
+     (Mean[Mean>2 & Mean<360], breaks=seq(2,360,by=1), 
+                                xlab = "Mean", main = "Histogram of Gene Expression Data Mean>2 & Mean<360"))
+dev.off()
+
+# We can also recommend using other data visualization tools to represent the frequencies of the means
+# like a scatterplot to accurately capture the data.
 
 # 6. Import this csv file into an R object.
 #    What are the column names?
